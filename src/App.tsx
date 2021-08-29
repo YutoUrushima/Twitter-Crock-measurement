793df42f0dc40f8ce0a7a4a788e275069f9b832c
yuto urushima <yuto259233@gmail.com>
2021-08-29 18:56:33 +0900

    data取得

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
+const App: React.FC = () => {
+  const handleFetchData = () => {
+    const fetchData = async () => {
+      const data = await fetch(
+        "https://api.twitter.com/2/tweets/1275828087666679809?tweet.fields=attachments,author_id,created_at,entities,geo,id,in_reply_to_user_id,lang,possibly_sensitive,referenced_tweets,source,text,withheld"
+      );
+      return data;
+    };
+    console.log(fetchData);
+    return fetchData;
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => handleFetchData()}>結果を見る</button>
     </div>
   );
-}
+};
 
 export default App;
