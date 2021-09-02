db49b8f85f726907c6c966bba574bf8fb7d5acf6
yuto urushima <yuto259233@gmail.com>
2021-09-02 21:23:37 +0900

    Fetchの記述追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,28 @@
-import React from "react";
-import "./App.css";
+import React from 'react';
+import './App.css';
+
+const token = process.env.REACT_APP_BEARER_TOKEN;
+// const endpoint = "https://api.twitter.com/2/tweets?ids=";
+const endpoint = 'https://api.twitter.com/2/users/2244994946';
 
 const App: React.FC = () => {
-  const handleFetchData = () => {
-    const fetchData = async () => {
-      const data = await fetch(
-        "https://api.twitter.com/2/tweets/1275828087666679809?tweet.fields=attachments,author_id,created_at,entities,geo,id,in_reply_to_user_id,lang,possibly_sensitive,referenced_tweets,source,text,withheld"
-      );
-      return data;
-    };
-    console.log(fetchData);
-    return fetchData;
+  const getData = async () => {
+    try {
+      const res = await fetch(endpoint, {
+        method: 'GET',
+        mode: "cors",
+        headers: {
+          "Authorization": `Bearer ${token}`
+        }
+      });
+      const json = await res.json();
+      return json;
+    } catch (error) {
+      console.log(error);
+    }
   };
-  return (
-    <div className="App">
-      <button onClick={() => handleFetchData()}>結果を見る</button>
-    </div>
-  );
+  console.log(getData());
+  return <></>;
 };
 
 export default App;
