732e921367a695c2048481a3fee4b1331fc18f37
Yuto Urushima <yuto259233@gmail.com>
2022-02-20 22:52:21 +0900

    yes or no APIをfetchして結果を表示

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import './App.css';
+import axios from 'axios';
+import { useState } from 'react';
 
 // const token = process.env.REACT_APP_BEARER_TOKEN;
 // const endpoint = "https://api.twitter.com/2/tweets?ids=";
 // const endpoint = 'https://api.twitter.com/2/users/2244994946';
 
 const App: React.FC = () => {
+  const [state, setState] = useState('');
+  const button = useRef(null)
+  const fetchData = async() => {
+    await axios.get(
+      "https://yesno.wtf/api"
+    )
+    .then((data) => {
+      setState(data.data.answer)
+      console.log(data.data.answer)
+    })
+  }
   return (
-    <div>Hello world</div>
+    <>
+      <p>result: {state}</p>
+      <button type="button" ref={button} onClick={fetchData}>push here</button>
+    </>
   )
 };
 
