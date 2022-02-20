8883be90b2b0457c165dd3d4e19009c1884d39c3
Yuto Urushima <yuto259233@gmail.com>
2022-02-20 22:33:17 +0900

    一旦、twitterAPI取得を取りやめ & コンソールで出ていた全エラーの解消

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,14 @@
 import React from 'react';
 import './App.css';
 
-const token = process.env.REACT_APP_BEARER_TOKEN;
+// const token = process.env.REACT_APP_BEARER_TOKEN;
 // const endpoint = "https://api.twitter.com/2/tweets?ids=";
-const endpoint = 'https://api.twitter.com/2/users/2244994946';
+// const endpoint = 'https://api.twitter.com/2/users/2244994946';
 
 const App: React.FC = () => {
-  const getData = async () => {
-    try {
-      const res = await fetch(endpoint, {
-        method: 'GET',
-        mode: "cors",
-        headers: {
-          "Authorization": `Bearer ${token}`
-        }
-      });
-      const json = await res.json();
-      return json;
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  console.log(getData());
-  return <></>;
+  return (
+    <div>Hello world</div>
+  )
 };
 
 export default App;
