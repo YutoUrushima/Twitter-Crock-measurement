2d3eaae4eb5d1d495a62ad19b157c752b2494959
Yuto Urushima <yuto259233@gmail.com>
2022-02-27 22:31:42 +0900

    AWS Lambdaからのデータのフェッチ

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,27 @@
 import React, { useRef } from 'react';
 import './App.css';
 import axios from 'axios';
-import { useState } from 'react';
 
-// const token = process.env.REACT_APP_BEARER_TOKEN;
-// const endpoint = "https://api.twitter.com/2/tweets?ids=";
-// const endpoint = 'https://api.twitter.com/2/users/2244994946';
+const url = "https://3yebf39eia.execute-api.ap-northeast-1.amazonaws.com/production/twitter-api"
 
 const App: React.FC = () => {
-  const [state, setState] = useState('');
   const button = useRef(null)
   const fetchData = async() => {
     await axios.get(
-      "https://yesno.wtf/api"
+      url,
+      {
+        params: {
+          name: "developer_japan"
+        }
+      }
     )
     .then((data) => {
-      setState(data.data.answer)
-      console.log(data.data.answer)
+      console.log(data.data)
     })
   }
   return (
     <>
-      <p>result: {state}</p>
+      <p>result</p>
       <button type="button" ref={button} onClick={fetchData}>push here</button>
     </>
   )
