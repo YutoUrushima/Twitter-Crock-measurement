eec4a7a45400b9a26944593c071e6c1313dca44b
Yuto Urushima <yuto259233@gmail.com>
2022-03-05 19:32:34 +0900

    prettierの有効化

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,30 @@
-import React, { useRef } from 'react';
-import './App.css';
-import axios from 'axios';
+import React, { useRef } from "react";
+import "./App.css";
+import axios from "axios";
 
-let url = `${process.env.REACT_APP_API_ENDPOINT_URL}/twitter-api`
+let url = `${process.env.REACT_APP_API_ENDPOINT_URL}/twitter-api`;
 
 const App: React.FC = () => {
-  const button = useRef(null)
-  const fetchData = async() => {
-    await axios.get(
-      url,
-      {
+  const button = useRef(null);
+  const fetchData = async () => {
+    await axios
+      .get(url, {
         params: {
           name: "developer_japan"
         }
-      }
-    )
-    .then((data) => {
-      console.log(data.data)
-    })
-  }
+      })
+      .then((data) => {
+        console.log(data.data);
+      });
+  };
   return (
     <>
       <p>result</p>
-      <button type="button" ref={button} onClick={fetchData}>push here</button>
+      <button type="button" ref={button} onClick={fetchData}>
+        push here
+      </button>
     </>
-  )
+  );
 };
 
 export default App;
